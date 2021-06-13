14c8ae7f14eaf6906d95bc2cf1dd045d41da2126
sernamar <sernamar@protonmail.com>
2021-06-13 08:59:54 +0200

    Add multithreading program that initializes an array using an index

--- C/p_normal_numbers.c
+++ C/p_normal_numbers.c
@@ -1,68 +1,57 @@
-#include <omp.h>
+#include <pthread.h>
 #include <stdio.h>
-#include <sys/time.h>
-#include <gsl/gsl_rng.h>
-#include <gsl/gsl_randist.h>
+#include <stdlib.h>
+#include <sys/sysinfo.h>
 
-#define NUMBER_OF_ELEMENTS 1
-#define MEAN 0
-#define STANDARD_DEVIATION 1
+#define NUMBER_OF_ELEMENTS 100
+#define NUMBER_OF_THREADS 4
+#define ELEMENTS_PER_THREAD NUMBER_OF_ELEMENTS / NUMBER_OF_THREADS
+
+double array[NUMBER_OF_ELEMENTS];
+
+void * initialize_array (void *thread_number) {
+        int *thread_id = (int *) thread_number;
+	int start = *thread_id * ELEMENTS_PER_THREAD;
+        int end = start + ELEMENTS_PER_THREAD;
+        printf("[Thread %d] Initializing elements from %d to %d\n", *thread_id, start, end-1);
+        for (int i = start; i < end; i++) {
+                array[i] = (double) i;
+        }
+        
+        pthread_exit(NULL);
+}
 
 int main (int argc, char *argv[])
 {
-        int number_of_elements;
-        
-        if (argc == 1) {
-                number_of_elements = NUMBER_OF_ELEMENTS;
-        } else if (argc == 2) {
-                number_of_elements = atoi(argv[1]);
-        } else {
-                fprintf(stderr, "%s\n", "Wrong number of arguments.");
-                fprintf(stderr, "%s\n", "Use: ./normal_numbers number_of_elements");
-                exit(EXIT_FAILURE);
-        }
+	pthread_t threads[NUMBER_OF_THREADS];
+	int thread_id[NUMBER_OF_THREADS];
+	int i;
 
-        double *numbers = malloc (number_of_elements * sizeof(double));
+        // create threads
+	for (i = 0; i < NUMBER_OF_THREADS; i++) {
+		thread_id[i] = i;
+                if(pthread_create(&threads[i], NULL, initialize_array, (void *) &thread_id[i]) == -1) {
+                        fprintf(stderr, "%s %d\n", "Can't create thread number", i);
+                }
+	}
 
-        double mean = MEAN;
-        double standard_deviation = STANDARD_DEVIATION;
-        
-        // declare the necessary random number generator variables
-        const gsl_rng_type *T;
-        gsl_rng *r;
-
-        // set the default values for the random number generator variables
-        gsl_rng_env_setup();
-
-        // create a seed based on time
-        struct timeval tv;
-        gettimeofday(&tv,0);
-        unsigned long seed = tv.tv_sec + tv.tv_usec;
-
-        // setup the generator using the seed just created
-        T = gsl_rng_default;
-        r = gsl_rng_alloc (T);
-        gsl_rng_set(r, seed);
-
-        // generate number_of_elements normal random numbers
-        #pragma omp parallel for
-        for (int i = 0; i < number_of_elements; i++) {
-                numbers[i] = mean + gsl_ran_gaussian (r, standard_deviation);
-        }
+	// wait until every thread ends
+	void *result;
+        for (i = 0; i < NUMBER_OF_THREADS; i++) {
+		if (pthread_join(threads[i], &result) == -1) {
+			printf("Can't join thread number %d\n", i);
+		}
+	}
 
         // print numbers
-        if(number_of_elements < 10) {
-                for (int i = 0; i < number_of_elements; i++) {
-                        printf (" %f", numbers[i]);
+        if(NUMBER_OF_ELEMENTS <= 100) {
+                for (int i = 0; i < NUMBER_OF_ELEMENTS; i++) {
+                        printf (" %f", array[i]);
                 }
                 printf("\n");
         } else {
                 printf("Done.\n");
         }
-
-        // free memory        
-        gsl_rng_free (r);
-        free(numbers);
   
         return 0;
 }
